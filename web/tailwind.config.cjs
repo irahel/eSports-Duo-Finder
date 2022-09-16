399c4840753ba49f3372f3d832c0dc1a35621a4e
Isaac Rahel <isaac_rahel@live.com>
2022-09-16 09:46:43 -0300

    chore: adjusts in layout

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,6 +5,9 @@ module.exports = {
     './index.html'
   ],
   theme: {
+    fontFamily:{
+      sans: ['Inter', 'sans-serif']
+    },
     extend: {
       backgroundImage: {
         'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 33.94%, #E1D55D 40.57%)',
