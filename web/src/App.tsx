4bc1a900540ed2ffbf2e86beea4d8fe66b3acc93
Isaac Rahel <isaac_rahel@live.com>
2022-09-16 08:59:36 -0300

    feat: init coding front web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,69 @@
+import './styles/main.css';
 
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props: ButtonProps){
-  return (
-    <button>
-      {props.title}
-    </button>
-  )
-}
-
+import logoImg from './assets/logo-nlw.svg';
 
 function App() {
   return (
-    <div>
-      <Button title="Enviar 1"/>
-      <Button title="Enviar 2"/>
-      <Button title="Enviar 3"/>
-      <Button title="Fala tu"/>
+    <div className = "max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="" />      
+
+      <h1 className = "text-6xl text-white font-black mt-20">
+        Seu  
+        <span className = "text-transparent bg-nlw-gradient bg-clip-text"> duo </span> 
+        está aqui.
+      </h1>
+
+      <div className = "grid grid-cols-6 gap-6 mt-16">
+        <a href="" className = "relative rounded-lg overflow-hidden">
+          <img src="/game1.png" alt="" />
+
+          <div className = "w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold block text-white">League of Legends</strong>
+            <span className = "text-zinc-300 text-sm block">12 anúncios</span>
+          </div>
+        </a>
+        <a href="" className = "relative rounded-lg overflow-hidden">
+          <img src="/game2.png" alt="" />
+
+          <div className = "w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold block text-white">DOTA 2</strong>
+            <span className = "text-zinc-300 text-sm block">10 anúncios</span>
+          </div>
+        </a>
+        <a href="" className = "relative rounded-lg overflow-hidden">
+          <img src="/game3.png" alt="" />
+
+          <div className = "w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold block text-white">CS Go</strong>
+            <span className = "text-zinc-300 text-sm block">6 anúncios</span>
+          </div>
+        </a>
+        <a href="" className = "relative rounded-lg overflow-hidden">
+          <img src="/game4.png" alt="" />
+
+          <div className = "w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold block text-white">Apex legends</strong>
+            <span className = "text-zinc-300 text-sm block">5 anúncios</span>
+          </div>
+        </a>
+        <a href="" className = "relative rounded-lg overflow-hidden">
+          <img src="/game5.png" alt="" />
+
+          <div className = "w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold block text-white">World of Warcraft</strong>
+            <span className = "text-zinc-300 text-sm block">3 anúncios</span>
+          </div>
+        </a>
+        <a href="" className = "relative rounded-lg overflow-hidden">
+          <img src="/game6.png" alt="" />
+
+          <div className = "w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold block text-white">Fortinite</strong>
+            <span className = "text-zinc-300 text-sm block">2 anúncios</span>
+          </div>
+        </a>
+  
+      </div>
     </div>
   )
 }
