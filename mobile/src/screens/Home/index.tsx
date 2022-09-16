ba28b5ef5b7557f6873532965da30d17a171ebd8
Isaac Rahel <isaac_rahel@live.com>
2022-09-16 15:53:40 -0300

    feat: coding mobile front

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,40 @@
+import { View, Image, FlatList } from 'react-native';
+import logoImg from '../../assets/logo-nlw-esports.png';
+import { styles } from './styles';
+import {Heading} from '../../components/Heading';
+import {GameCard} from '../../components/GameCard';
+import { GAMES } from '../../utils/games'
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image 
+      source={logoImg}
+      style = {styles.logo}
+      />
+
+      <Heading
+      title="Encontre seu duo!"
+      subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <FlatList 
+      data = {GAMES}
+      keyExtractor={item => item.id}
+      horizontal
+      contentContainerStyle={styles.contentlist}
+      showsHorizontalScrollIndicator={false}
+      renderItem={({item}) => (      
+      <GameCard
+        data={
+          item
+        }
+        />)}
+      />
+
+
+
+
+    </View>
+  );
+}
